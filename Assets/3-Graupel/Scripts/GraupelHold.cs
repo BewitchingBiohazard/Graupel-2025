9515d791ea2b7cf90d1913dec531ecd5a42a8c2b
tessajohnson005 <tessajohnson005@gmail.com>
2025-12-18 21:41:52 -0600

    Added moving tongue

---
+++ Assets/3-Graupel/Scripts/GraupelHold.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Graupel
+{
+    public class GraupelPlayer : MonoBehaviour
+    {
+        public Slider Slider_Main;
+
+        public float changeAmount = 50f;
+
+        void Start()
+        {
+            Slider_Main.minValue = 0f;
+            Slider_Main.maxValue = .5f;
+            Slider_Main.value = 0f;
+        }
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                Debug.Log("E Pressed");
+                Slider_Main.value += changeAmount * Time.deltaTime;
+            }
+            else
+            {
+                Slider_Main.value -= changeAmount * Time.deltaTime;
+            }
+
+            Slider_Main.value = Mathf.Clamp(Slider_Main.value, 0f, .5f);
+        }
+    }
+}
+
+
+
+
